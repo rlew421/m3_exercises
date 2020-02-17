69367e8c4d3ed1cdbcd66ad4a5fcde66d1a5decf
Rachel Lew <rlew07@gmail.com>
2020-02-17 14:15:15 -0700

    Move getting/parsing of JSON into private method

--- api_consumption/lib/marvel_service.rb
+++ api_consumption/lib/marvel_service.rb
@@ -3,13 +3,16 @@ require 'pry'
 
 class MarvelService
   def character(name)
-    response = conn.get("characters?apikey=037af0662eeded75ad64e038edfdc735&hash=537bb5672fe49770e74c2b4ee329e517&ts=1581883411&name=#{name}")
-
-    json = JSON.parse(response.body)
+    get_json("characters?apikey=037af0662eeded75ad64e038edfdc735&hash=537bb5672fe49770e74c2b4ee329e517&ts=1581883411&name=#{name}")
   end
 
   private
-  
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body)
+  end
+
   def conn
     Faraday.new(url: "http://gateway.marvel.com/v1/public/") do |faraday|
       faraday.adapter Faraday.default_adapter
