1c04242c5f996d6ca0123526871c7d646fedb0e0
Rachel Lew <rlew07@gmail.com>
2020-02-17 15:07:18 -0700

    Add timestamp and hash_digest private methods for dynamic query params

--- api_consumption/lib/marvel_service.rb
+++ api_consumption/lib/marvel_service.rb
@@ -1,12 +1,33 @@
+require 'digest'
 require 'faraday'
 require 'pry'
 
 class MarvelService
   def character(name)
-    get_json("characters?apikey=037af0662eeded75ad64e038edfdc735&hash=537bb5672fe49770e74c2b4ee329e517&ts=1581883411&name=#{name}")
+    get_json("characters?apikey=037af0662eeded75ad64e038edfdc735&hash=#{hash_digest}&ts=#{timestamp}&name=#{name}")
+  end
+
+  # def creators(first_name: first_name)
+  #   get_json("creators?apikey=037af0662eeded75ad64e038edfdc735&hash=#{hash_digest}&ts=#{timestamp}&firstName=#{first_name}")
+  # end
+
+  def creators(last_name: last_name)
+    get_json("creators?apikey=037af0662eeded75ad64e038edfdc735&hash=#{hash_digest}&ts=#{timestamp}&lastName=#{last_name}")
   end
 
   private
+
+  def timestamp
+    Time.now.to_i
+  end
+
+  def hash_digest
+    private_key = "1914f30557567f3b202b98d61bf1182d5903c17c"
+
+    public_key = "037af0662eeded75ad64e038edfdc735"
+
+    Digest::MD5.hexdigest "#{timestamp}#{private_key}#{public_key}"
+  end
 
   def get_json(url)
     response = conn.get(url)
